590b50886e871317a37cf597d3633bf10141673f
Pinenut <973301529@qq.com>
2017-09-18 23:27:34 +0800

    first entry

---
+++ app/src/main/java/com/example/andorid/scos/es/source/code/activity/SCOSEntry.java
@@ -0,0 +1,15 @@
+package com.example.andorid.scos.es.source.code.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andorid.scos.R;
+
+public class SCOSEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scosentry);
+    }
+}
---
+++ app/src/main/java/com/example/andorid/scos/hahha.java
@@ -0,0 +1,8 @@
+package com.example.andorid.scos;
+
+/**
+ * Created by 97330 on 2017/9/17.
+ */
+
+public class hahha {
+}
